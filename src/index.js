bbabb6fc361948299316721e6ada84d03eec597e
Bansal <cbansal@monq.ad>
2019-12-06 10:49:25 +0530

    webpack init

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
-var totalMultiply = multiply(5, 3);
-var totalSum = sum(5, 3);
-console.log('Product of 5 and 3 = ' + totalMultiply);
-console.log('Sum of 5 and 3 = ' + totalSum);
+// var totalMultiply = multiply(5, 3);
+// var totalSum = sum(5, 3);
+// console.log('Product of 5 and 3 = ' + totalMultiply);
+// console.log('Sum of 5 and 3 = ' + totalSum);
+
+// var multiply = require('./multiply');
+// var sum = require('./sum');
+// var totalMultiply = multiply(5, 3);
+// var totalSum = sum(5, 3);
+// console.log('Product of 5 and 3 = ' + totalMultiply);
+// console.log('Sum of 5 and 3 = ' + totalSum);
+
+import multiply from './multiply';
+import sum from './sum';
+const totalMultiply = multiply(5, 3);
+const totalSum = sum(5, 3);
+console.log(`Product of 5 and 3 = ${totalMultiply}`);
+console.log(`Sum of 5 and 3 = ${totalSum}`);
--- src/sum.js
+++ src/sum.js
@@ -1,3 +1,11 @@
-var sum = function (a, b) {
-    return a + b;
-};
+// var sum = function (a, b) {
+//     return a + b;
+// };
+
+// var sum = function (a, b) {
+//     return a + b;
+// };
+// module.exports = sum;
+
+const sum = (a, b) => a + b;
+export default sum;
